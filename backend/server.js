6503abe06fd2427bcd1de44c74e9cac77011eca8
Masine Donald <masinedonald@gmail.com>
2026-06-21 05:45:35 +0200

    Change server port from 5001 to 5000

--- backend/server.js
+++ backend/server.js
@@ -475,7 +475,7 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
